0b8f8e55cd847f65af2a4ab2df0bebe8f26a5916
Jill Zakarian <JillianZakarian@gmail.com>
2022-09-13 07:01:30 -0400

    added eventlistener on btn to change grid size

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 "use strict";
 
+let cell = document.querySelector(".cell");
+let container1 = document.getElementById("container");
+let button = document.getElementById("btn");
+
+let gridSize = function (number) {
 let i = 0,
-  r = 16;
+repeat = number;
 
 container.innerHTML = `<div class="row">${'<div class="cell"> </div>'.repeat(
-  r
-)}</div>`.repeat(r);
+  number
+)}</div>`.repeat(number);
+}
+
+gridSize(16);
+
+container1.addEventListener("mouseover", (event) => {
+  event.target.style.color = "purple";
+});
+
+button.addEventListener("click", function () {
+  let userInput = prompt("Enter number of squares per side of grid");
+  console.log(Number(userInput));
+  if (!Number(userInput)) {
+    alert("Enter only numbers. Try again.");
+   }  else {
+    gridSize(userInput);
+    }
+});
