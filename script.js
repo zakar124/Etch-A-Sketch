467df3f0bfb77d3acc65cb2aeb79a73664bfd90c
Jill Zakarian <JillianZakarian@gmail.com>
2022-09-13 17:47:03 -0400

    used css hover, must revise with an eventlistener

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
 "use strict";
 
-let cell = document.querySelector(".cell");
-let container1 = document.getElementById("container");
+// let cell = document.querySelectorAll(".cell");
+let containerMain = document.getElementById("container");
 let button = document.getElementById("btn");
+let row = document.querySelector(".row");
+
+
 
 let gridSize = function (number) {
-  let i = 0,
-    repeat = number;
+  let i = 0, repeat = number;
   number = Math.min(100, Math.max(0, number));
+  containerMain.innerHTML =
+    `<div class="row">${'<div class="cell"></div>'.repeat(
+      number
+    )}</div>`.repeat(number);
 
-  container.innerHTML = `<div class="row">${'<div class="cell"> </div>'.repeat(
-    number
-  )}</div>`.repeat(number);
 };
 
 gridSize(16);
 
-container1.addEventListener("mouseover", (event) => {
-  event.target.style.color = "purple";
-});
 
 button.addEventListener("click", function () {
   let userInput = prompt("Enter number of squares 😁");
-  console.log(Number(userInput));
   if (!Number(userInput)) {
     alert("Enter only numbers. Try again 🙃");
   } else {
